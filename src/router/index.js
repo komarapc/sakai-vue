f82f70da09416c761c717fb3c1e2f44b84008e37
izmi.komar <komar.izmi@gmail.com>
2024-05-16 03:30:59 +0700

    feat: Add dynamic title bar

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import AppLayout from '@/layout/AppLayout.vue';
-
+import { capitalize } from 'vue';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -72,6 +72,7 @@ const router = createRouter({
                 {
                     path: '/uikit/menu',
                     component: () => import('@/views/uikit/Menu.vue'),
+                    name: 'menu',
                     children: [
                         {
                             path: '/uikit/menu',
@@ -170,6 +171,11 @@ const router = createRouter({
             component: () => import('@/views/pages/auth/Error.vue')
         }
     ]
+});
+
+router.beforeEach((to, from, next) => {
+    document.title = `Primevue - ${capitalize(to.name)}`;
+    next();
 });
 
 export default router;
